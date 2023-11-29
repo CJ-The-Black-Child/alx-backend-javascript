08d09ed0f0aa9842813db9b59a62980252245b17
CJ-The-Black-Child <congomigue@gmail.com>
2023-11-30 02:42:12 +0300

    eslint bug fixes and changes

---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,3 @@
+# Title: ES6 Basics
+## Description
+This directory contains all the know how on getting started with modern js
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
+  const queue = [];
 
-    try {
-        queue.push(mathFunction());
-    } catch (error) {
-        queue.push(String(error));
-    } finally {
-        queue.push('Guardrail was processed');
-    }
-    return queue;
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
