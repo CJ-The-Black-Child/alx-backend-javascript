7ccdb07cb5abdf4672eeb777bd03ed5fe94782d8
CJ-The-Black-Child <congomigue@gmail.com>
2023-11-30 00:38:31 +0300

    fixed bugs and syntax issues

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    let res = {}
+    let res = {};
 
     try {
         const photo = await uploadPhoto();
