eff81c132b902fea74684ac81784fcd2f6555e92
CJ-The-Black-Child <congomigue@gmail.com>
2023-12-07 00:35:23 +0300

    Brand convention & Nominal typing

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,15 @@
+export interface MajorCredits {
+ credits: number & {__brand: 'MajorCredits.credits'};   
+}
+
+export interface MinorCredits {
+  credits: number & {__brand: 'MinorCredits.credits'};
+}
+
+export function sumMajorCredits(subject1: MajorCredits, subjects2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subjects2.credits } as MajorCredits;
+}
+
+export function sumMinorCredits(subject1: MinorCredits, subjects2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subjects2.credits } as MinorCredits;
+}
