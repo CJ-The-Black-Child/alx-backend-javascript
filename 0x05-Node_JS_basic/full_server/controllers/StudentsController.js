1e1c043842d7da2e4caae2352bbec5e63dda466d
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-30 13:18:51 +0300

    Organizes a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        const students = data.split('\n')
+                            .filter(Boolean)
+                            .map((item) => item.split(','));
+        const fields = students.reduce((acc, student, i) => {
+            if (i !== 0) {
+                if (!acc[student[3]]) acc[student[3]] = [];
+                acc[student[3]].push(student[0]);
+            }
+            return acc;
+        }, {});
+
+        delete fields.field;
+
+        resolve(fields);
+    });
+});
+
+module.exports = readDatabase;
