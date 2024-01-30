268c1e1237993558911c48cc6595fec19a707bc1
CJ-The-Black-Child <congomigue@gmail.com>
2024-01-30 13:17:37 +0300

    Sets up a server and counts students

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = process.argv[2];
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	
+	if (req.url === '/') {
+		res.end('Hello Holberton School!');
+	} else if (req.url === '/students') {
+		try {
+			const students = await countStudents(DATABASE);
+			res.end(`This is the list of our students\n${students.join('\n')}`);
+		} catch (error) {
+			res.end(error.message);
+		}
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
+});
+
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
